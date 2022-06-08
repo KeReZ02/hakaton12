c3e54dffec3029e572761ac896aad90a06937859
KeReZ02 <bekbolotovakerez@gmail.com>
2022-06-08 16:05:16 +0600

    'inital'

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.slider_general').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoPlay:true,
+    autoplaySpeed:500
+});
+$('.images').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    leftMode: true,
+    variableWidth: true
+});
